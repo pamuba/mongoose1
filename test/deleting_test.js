5780a79f5ff8b72731dd0d4975c13f46405fb9aa
pamuba <pamuba@users.noreply.github.com>
2020-07-28 21:28:35 +0530

    3

--- test/deleting_test.js
+++ test/deleting_test.js
@@ -1,30 +1,30 @@
-const mocha = require('mocha')
-const assert = require('assert')
-const MarioChar = require('../models/mariochar')
+// const mocha = require('mocha')
+// const assert = require('assert')
+// const MarioChar = require('../models/mariochar')
 
-//test suits--collection of related tests
-describe('Deleting Records', function() {
+// //test suits--collection of related tests
+// describe('Deleting Records', function() {
 
-    var char;
-    //Create Tests
-    beforeEach(function(done){
-        char= new MarioChar({
-            name:'Mario'
-        });
+//     var char;
+//     //Create Tests
+//     beforeEach(function(done){
+//         char= new MarioChar({
+//             name:'Mario'
+//         });
 
-        char.save().then(function(){
-            assert(char.isNew === false)
-            done();
-        });
-    });
+//         char.save().then(function(){
+//             assert(char.isNew === false)
+//             done();
+//         });
+//     });
 
-    it('Delete one record from the database', function(done){
-        MarioChar.findOneAndRemove({name:"Mario"}).then(function(){
-            MarioChar.findOne({name:'Mario'}).then(function(result){
-                assert(result === null)
-                done();
-            })
-        })
-    })
-});
+//     it('Delete one record from the database', function(done){
+//         MarioChar.findOneAndRemove({name:"Mario"}).then(function(){
+//             MarioChar.findOne({name:'Mario'}).then(function(result){
+//                 assert(result === null)
+//                 done();
+//             })
+//         })
+//     })
+// });
     
